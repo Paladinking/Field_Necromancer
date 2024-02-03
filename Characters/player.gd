6213da38d14043dea7db408e42f1b45fd2275d02
Filip Berg <filbe316@student.liu.se>
2024-02-03 13:13:02 +0100

    camera follows player

--- Characters/player.gd
+++ Characters/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
+const SPEED = 10.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
--- main.gd
+++ main.gd
@@ -1,16 +1,18 @@
 extends Node
 
+const CAMERA_OFFSET = Vector3(0, 100, 42)
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Grave.raise()
 	$Grave.creature_to_spawn = preload("res://Characters/zombie.tscn")
-	pass # Replace with function body.
+	pass # Replace with function body.sawdsaaaaads
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	$Camera3D.position = $Player.position + CAMERA_OFFSET
 
 
 func _unhandled_key_input(event):
