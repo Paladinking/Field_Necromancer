f654bcfb37722fab2b3d266f405e2f5d732c0497
Filip Berg <filbe316@student.liu.se>
2024-02-04 11:00:26 +0100

    pre-merge

--- Characters/zombie.gd
+++ Characters/zombie.gd
@@ -1,6 +1,7 @@
 extends Fighter
 
 const FOLLOW_TIME : float = 5.0
+const WALKING_ANIMATION = "March"
 
 # Player or null
 var _following_time: float = 0.0
@@ -8,16 +9,20 @@ var _following: Node3D
 
 var _has_target_location : bool = false
 
+
+
 func set_target_location(pos: Vector3):
 	_nav_agent.set_target_position(pos)
 	_has_target_location = true
 	_following_time = FOLLOW_TIME
+
 
 func set_following(goal: Node3D):
 	_following = goal
 	_following_time = FOLLOW_TIME
 	_nav_agent.set_target_position(_following.position)
 	_has_target_location = false
+
 
 func _ready():
 	super._ready()
@@ -27,6 +32,7 @@ func _ready():
 			_following_time = 0.0
 			find_target()
 	)
+
 
 func _physics_process(delta: float):
 	if _following_time > 0.0:
@@ -40,5 +46,9 @@ func _physics_process(delta: float):
 		var next_path_position: Vector3 = _nav_agent.get_next_path_position()
 		velocity = global_position.direction_to(next_path_position) * SPEED
 		_finalize_move(delta)
+		if not $zombie/AnimationPlayer.is_playing() and not velocity.is_zero_approx():
+			$zombie/AnimationPlayer.play(WALKING_ANIMATION)
+		elif $zombie/AnimationPlayer.assigned_animation == WALKING_ANIMATION and $zombie/AnimationPlayer.is_playing():
+			$zombie/AnimationPlayer.pause()
 	else:
 		super._physics_process(delta)
